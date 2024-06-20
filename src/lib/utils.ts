9b8e97ca3e73a928813f7c331723335c1417d3c4
MorrisRio <maurice.rio@gmx.de>
2024-06-20 23:32:13 +0200

    test 2 debug vercel api endpoints

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,11 +11,20 @@ export const debugLog = (...args: any) => {
 };
 
 export const getAppUrl = () => {
-    if (process.env.NEXTAUTH_URL) {
-        return process.env.NEXTAUTH_URL;
-    } else if (process.env.VERCEL_URL) {
-        return `https://${process.env.VERCEL_URL}`;
-    } else {
+    try {
+        if (process.env.NEXTAUTH_URL) {
+            return process.env.NEXTAUTH_URL;
+        } else if (process.env.VERCEL_ENV === "preview") {
+            return `https://playlist-labs-git-dev-maurices-projects-1e3466ea.vercel.app/api/auth/signin`;
+        } else if (process.env.VERCEL_URL) {
+            return `https://${process.env.VERCEL_URL}`;
+        } else {
+            throw new Error(
+                "Failed to find a a valid URL for the app. Add NEXTAUTH_URL to your environment variables."
+            );
+        }
+    } catch (error) {
+        console.error(error);
         return "missing-url";
     }
 };
