46e79381bdaa3200a6166f5e5085fe08812f96eb
MorrisRio <maurice.rio@gmx.de>
2024-09-10 22:54:32 +0200

    v0.3 cron Endpoint

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,19 +2,26 @@
 import { NextResponse, NextRequest } from "next/server";
 import { dbGetAccountByUserId, dbGetAllUsers } from "@/lib/db/dbActions";
 import { refreshAccessToken, updateAccountTokenInDb } from "@/lib/auth";
-import { JWT } from "next-auth/jwt";
-import { setDebugMode, debugLog } from "@/lib/utils"
+import { setDebugMode, debugLog, getAppUrl } from "@/lib/utils";
+import { spotifyPut } from "@/lib/serverUtils";
 
 export async function GET(req: NextRequest) {
     setDebugMode(true);
+    const now = Date.now();
 
     const usersFromDb = await dbGetAllUsers();
-    if(usersFromDb.error || !usersFromDb.data) {
-        return NextResponse.json({message: "failed getting userdata"}, {status: 500});
-    }
 
+    if (usersFromDb.error || !usersFromDb.data) {
+        return NextResponse.json({ message: "failed getting userdata" }, { status: 500 });
+    }
     const users = usersFromDb.data;
-    users.forEach( async(user) => {
+    let playlistCount = 0;
+    let fetchCount = 0;
+    let userCount = 0;
+
+    for (const user of users) {
+        console.log("User: " + user.name);
+        userCount++;
         const accountFromDB = await dbGetAccountByUserId(user.spotify_id);
         if (accountFromDB.error || accountFromDB.data === null) {
             console.error("No account found for user " + user.spotify_id);
@@ -26,9 +33,29 @@ export async function GET(req: NextRequest) {
             let refreshToken = await refreshAccessToken({
                 accessToken: access_token,
                 refreshToken: refresh_token,
-            } as JWT);
-            updateAccountTokenInDb(accountDB, refreshToken);
+                userId: user.spotify_id,
+                accessTokenExpires: token_expires,
+            });
+            if (refreshToken) updateAccountTokenInDb(accountDB, refreshToken);
         }
-    });
-    return NextResponse.json({ data: playlists, message: "Hello World" });
+        for (const playlist of user.playlists) {
+            const { playlist_id, preferences, seeds, rules } = playlist;
+            playlistCount++;
+            console.log("Playlist: " + playlist.preferences.name);
+            //TODO: extraxt playlistcreation and update to a function
+            await fetch(getAppUrl() + "/api/spotify/playlist", {
+                method: "PUT",
+                body: JSON.stringify({
+                    playlist_id,
+                    preferences,
+                    seeds,
+                    rules,
+                }),
+            });
+        }
+    }
+
+    const message = `updated ${playlistCount} Playlist for ${userCount} Users with ${fetchCount} fetches`;
+    console.log(message);
+    return NextResponse.json({ data: message }, { status: 200 });
 }
