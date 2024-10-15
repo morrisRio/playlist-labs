dfa11b71436e7352b342455680e4d5c0d88ef1e6
MorrisRio <maurice.rio@gmx.de>
2024-10-15 23:54:15 +0200

    performance TransitionLink

--- src/components/TransitionLink.tsx
+++ src/components/TransitionLink.tsx
@@ -1,12 +1,8 @@
 "use client";
-import { useState, ReactNode, useEffect, useRef, useCallback } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import Link, { LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
 import { useRouter } from "nextjs-toploader/app";
-
-import Lottie from "lottie-react";
-import Loading from "@/lib/lotties/loading.json";
-import { useTimeout } from "@/lib/hooks/useTimeout";
 
 interface TransitionLinkProps extends LinkProps {
     children: ReactNode;
@@ -22,23 +18,19 @@ export default function TransitionLink({ children, href, enter = false, ...props
     const router = useRouter();
     const pathname = usePathname();
 
-    // const enterRef = useRef<boolean>(enter);
-    const enterRef = useRef<boolean>(enter);
-    const hrefRef = useRef<string | null>(null);
-    // console.log(href + " enterState in client: ", enterState);
+    const navigateToRef = useRef<string | null>(null);
+
     const handleTransition = async (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
         e.preventDefault();
         const main = document.querySelector("main");
-        hrefRef.current = href;
+        navigateToRef.current = href;
 
         //fade out to the left
-        if (enterRef.current) main?.classList.add("page-transition-left");
+        if (enter) main?.classList.add("page-transition-left");
         else main?.classList.add("page-transition-right");
 
         //sleep for animation duration
-        console.log("sleep for 1000ms");
-        await sleep(1000);
-        console.log("slept for 1000ms");
+        await sleep(100);
 
         //navigate to the new page
         console.log("router pushing");
@@ -48,34 +40,29 @@ export default function TransitionLink({ children, href, enter = false, ...props
     useEffect(() => {
         //use effect runs after the navigation is done
         return () => {
-            //when it was previously shown hide it again
             async function animateBack() {
-                console.log("href:", href);
-                console.log("hrefRef:", hrefRef.current);
-                console.log("pathname: ", pathname);
-                console.log("enterRef: ", enterRef.current);
                 const main = document.querySelector("main");
                 if (!main) return;
 
+                //siwtch to opposite sides
                 main.style.transitionProperty = "none";
-                await sleep(10);
-                if (enterRef.current) {
+                if (enter) {
                     main.classList.remove("page-transition-left");
                     main.classList.add("page-transition-right");
                 } else {
                     main.classList.add("page-transition-left");
                     main.classList.remove("page-transition-right");
                 }
-                await sleep(10);
+                await sleep(5);
 
                 console.log(href + " reseting transition property to 1000ms");
                 main.style.transitionProperty = "opacity, filter, transform";
-                await sleep(10);
-                if (enterRef.current) main?.classList.remove("page-transition-right");
+
+                if (enter) main?.classList.remove("page-transition-right");
                 else main?.classList.remove("page-transition-left");
             }
 
-            if (hrefRef.current === href) animateBack();
+            if (navigateToRef.current === href) animateBack();
         };
     }, []);
 
