5a3f25076ad3e861b33b51cdede356accb203adb
MorrisRio <maurice.rio@gmx.de>
2024-10-24 03:34:00 +0200

    dont cache dbConnection in production

--- src/lib/db/dbConnect.ts
+++ src/lib/db/dbConnect.ts
@@ -2,6 +2,12 @@ import { ServerApiVersion } from "mongodb";
 import mongoose from "mongoose";
 import { debugLog, setDebugMode } from "@/lib/utils";
 
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+}
+
 declare global {
     var mongoose: any;
 }
@@ -11,43 +17,41 @@ global.mongoose = {
     promise: null,
 };
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
-}
-
 let cached = global.mongoose;
 
 if (!cached) {
     cached = global.mongoose = { conn: null, promise: null };
 }
 
-//TODO: PRODUCTION: dont cache connection
 export const connectMongoDB = async () => {
-    if (cached.conn) {
+    const options = {
+        autoIndex: true,
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        },
+    };
+
+    if (process.env.VERCEL_ENV === "production") {
+        return await mongoose.connect(MONGODB_URI, options);
+    } else {
+        if (cached.conn) {
+            return cached.conn;
+        }
+
+        if (!cached.promise) {
+            cached.promise = mongoose.connect(MONGODB_URI, options).then((mongoose) => {
+                return mongoose;
+            });
+        }
+        try {
+            cached.conn = await cached.promise;
+        } catch (e) {
+            debugLog("DB: Error connecting to MongoDB: ", e);
+            cached.promise = null;
+            throw e;
+        }
         return cached.conn;
     }
-
-    if (!cached.promise) {
-        const options = {
-            autoIndex: true,
-            serverApi: {
-                version: ServerApiVersion.v1,
-                strict: true,
-                deprecationErrors: true,
-            },
-        };
-        cached.promise = mongoose.connect(MONGODB_URI, options).then((mongoose) => {
-            return mongoose;
-        });
-    }
-    try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        debugLog("DB: Error connecting to MongoDB: ", e);
-        cached.promise = null;
-        throw e;
-    }
-    return cached.conn;
 };
